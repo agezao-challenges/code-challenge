577457b2db73ab41df86eae8f8085eced6feae99
Age <0800.grc@gmail.com>
2020-07-23 15:59:24 -0300

    Migrating apollo to a different infrastructure context, addim theme and colormode providers and adding the router import to index

--- src/index.tsx
+++ src/index.tsx
@@ -1,32 +1,25 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { ThemeProvider, ColorModeProvider, CSSReset } from '@chakra-ui/core';
+import { ApolloProvider } from '@apollo/client';
+import apolloClient from './infrastructure/apollo-client';
 import * as serviceWorker from "./serviceWorker";
-import {
-  ApolloClient,
-  HttpLink,
-  InMemoryCache,
-  ApolloProvider,
-} from "@apollo/client";
-
-const client = new ApolloClient({
-  cache: new InMemoryCache(),
-  link: new HttpLink({
-    uri: "https://api.blocktap.io/graphql",
-  }),
-});
+import { Router } from './routes';
+import './index.scss';
 
 ReactDOM.render(
   <React.StrictMode>
-    <ApolloProvider client={client}>
-      <App />
-    </ApolloProvider>
+    <ThemeProvider>
+      <ColorModeProvider>
+        <CSSReset />
+
+        <ApolloProvider client={apolloClient}>
+          <Router />
+        </ApolloProvider>
+      </ColorModeProvider>
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
