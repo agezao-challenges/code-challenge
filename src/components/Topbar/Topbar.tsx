0083a6dbd5b53c06410950e34172dc899a96a48a
Age <0800.grc@gmail.com>
2020-07-23 16:01:12 -0300

    Adding a topbar component to control theme color

---
+++ src/components/Topbar/Topbar.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import {
+  useColorMode,
+  Box,
+  IconButton,
+} from "@chakra-ui/core";
+import { RiSunLine, RiMoonLine } from 'react-icons/ri';
+
+export default (): React.ReactElement => {
+  const { colorMode, toggleColorMode } = useColorMode();
+
+  console.log(colorMode);
+
+  return (
+    <Box
+      px={6}
+      py={3}
+      d="flex"
+      justifyContent="flex-end"
+    >
+      <IconButton
+        variant="outline"
+        aria-label="Menu"
+        icon={colorMode === 'light' ? RiMoonLine : RiSunLine}
+        fontSize="22px"
+        onClick={toggleColorMode}
+      />
+    </Box>
+  );
+}
