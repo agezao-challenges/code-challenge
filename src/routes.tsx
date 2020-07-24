b4e5c4fbd0dbcc67ecc07ba0f2f78b84376cf37d
Age <0800.grc@gmail.com>
2020-07-24 01:31:13 -0300

    Adding new SymbolMarkets routes to the routing component

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import { Home } from './pages';
+import { Home, SymbolMarkets } from './pages';
 
 export const Router: React.FC = () => {
   return (
     <BrowserRouter>
       <Switch>
         <Route exact path='/' component={ Home } />
+        <Route exact path='/currencies/:symbol/markets' component={ SymbolMarkets } />
       </Switch>
     </BrowserRouter>
   );
