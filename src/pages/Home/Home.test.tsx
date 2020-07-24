7e65ce1cfbc4d0ea757d51ab08214f8c852bc201
Age <0800.grc@gmail.com>
2020-07-24 01:28:47 -0300

    Adding graphql mocks to Home page tests

--- src/pages/Home/Home.test.tsx
+++ src/pages/Home/Home.test.tsx
@@ -1,9 +1,49 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render } from '../../utils/test-utils';
+import { MockedProvider } from '@apollo/client/testing';
+import { GET_ASSETS } from '../../services/queries/GET_ASSETS'
 import Home from './Home';
 
-test('renders greeting message', () => {
-  const { getByText } = render(<Home />);
-  const greetingElement = getByText(/cool/i);
+const mocks = [
+  {
+    request: {
+      query: GET_ASSETS,
+      variables: {
+        limit: 25,
+        searchQuery: '%%'
+      },
+    },
+    result: {
+      "data": {
+        "assets": [
+          {
+            "assetName": "NEO",
+            "assetSymbol": "NEO",
+            "marketCap": 856432075,
+            "marketCapRank": 19,
+            "markets": [
+              {
+                "ticker": {
+                  "lastPrice": "11.27510000",
+                  "baseVolume": "157048.50010000"
+                }
+              },
+            ]
+          },
+        ]
+      }
+    },
+  },
+];
+
+test('renders the Home component and its dependencies', () => {
+  const { getByText } = render(
+    <MockedProvider mocks={mocks}>
+      <Home />
+    </MockedProvider>,
+  );
+
+  const greetingElement = getByText(/Cryptocurrency Market/i);
   expect(greetingElement).toBeInTheDocument();
 });
+
